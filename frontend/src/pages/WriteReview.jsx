bc4a0aae57a9da767b6c7e0dced931b10bbe42ac
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-07-11 01:16:00 +0900

    Update WriteReview.jsx

--- frontend/src/pages/WriteReview.jsx
+++ frontend/src/pages/WriteReview.jsx
@@ -225,9 +225,11 @@ export default function WriteReview() {
       {!currentUser && ( 
         <div className="notice-box">
         로그인 후 배정받은 상품을 선택해주세요.<br />
-        회원가입 시 전화번호는 숫자만 입력하세요.<br />
-        회원가입은 지금 본인계정 이름과 전화번호로 가입하시고<br />
-        계정 추가 시 실제 진행 계정을 입력하셔서 등록하세요.
+        회원가입 시 전화번호는 숫자만 입력하세요.<br /><br />
+        회원가입은 지금 본인 이름과 전화번호로 가입하시고<br />
+        계정 추가 시 실제 진행 계정을 입력하셔서 등록하세요.<br />
+        * 타계정도 본인 이름과 전화번호로 가입하셔야합니다. *<br />
+        지금 가입하시는 하나의 이름과 전화번호로 전부 관리하는겁니다.
         </div> )}
       {isLoginModalOpen && <LoginModal onClose={() => setIsLoginModalOpen(false)} onLoginSuccess={handleLoginSuccess} />}
       {currentUser ? (
