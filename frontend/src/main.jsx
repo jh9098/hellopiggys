efad8b2f55d5b31ec4096ce1b633c406464e573c
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-07-21 21:15:08 +0900

    Add PWA support

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 import './index.css'; // [추가] Tailwind CSS를 포함한 전역 스타일을 불러옵니다.
 
-// ▼▼▼ 디버깅을 위해 이 코드를 추가 ▼▼▼
-console.log("읽어온 API 키:", import.meta.env.VITE_FIREBASE_API_KEY);
-// ▲▲▲ 추가 완료 ▲▲▲
+registerSW();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,32 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path'; // [추가] Node.js의 'path' 모듈을 임포트
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Review Platform',
+        short_name: 'Review',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
