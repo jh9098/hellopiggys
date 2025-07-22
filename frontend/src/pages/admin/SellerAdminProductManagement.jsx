5c11cb5c19c6c5e7cfcbb2129ad82ffd689fa79c
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-07-22 22:20:19 +0900

    Fix grouped table closing

--- frontend/src/pages/admin/SellerAdminProductManagement.jsx
+++ frontend/src/pages/admin/SellerAdminProductManagement.jsx
@@ -400,7 +400,7 @@ export default function AdminProductManagementPage() {
                     </tr>
                   );
                   })
-                )}
+                ))}
             </tbody>
           </table>
         )}
